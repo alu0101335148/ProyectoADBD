ed29e451e2684e152216b25101c88b5e65e5fa0d
Airam Rafael Luque León <alu0101335148@ull.edu.es>
2023-01-08 11:18:50 +0000

    API para la control de stock agregada y cambios menores
    Co-authored-by: Andrés Pérez <andres.perez.11@ull.edu.es>

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from enum import Enum
 from typing import Union, List
 from datetime import date
 import os
@@ -30,6 +31,15 @@ def root():
 
 class Producto(BaseModel):
     id_prod: int
+    nombre: str
+    distribuidor: str
+    marca: str
+    precio: float
+    descripcion: str
+    categoria: str
+    fecha_caducidad: Union[date, None] = None
+
+class ProductoInsert(BaseModel):
     nombre: str
     distribuidor: str
     marca: str
@@ -108,7 +118,7 @@ def get_product(product_id: int) -> Producto:
             conn.close()
 
 @app.post("/products/", tags=["products"])
-def insert_products(new_product: Producto):
+def insert_products(new_product: ProductoInsert):
     conn = None
     try:
         conn = get_db_connection()
@@ -779,3 +789,181 @@ def delete_warehouse(warehouse_id: str):
 
 # *****************************************************************************
 
+class Destino(str, Enum):
+    TIENDA = "Tienda"
+    ALMACEN = "Almacen"
+
+class DisponibilidadTienda(BaseModel):
+    id_tie: str
+    id_prod: int
+    cantidad: int
+
+class DisponibilidadAlmacen(BaseModel):
+    id_alm: str
+    id_prod: int
+    cantidad: int
+
+class DisponibilidadUpdate(BaseModel):
+    id_prod: str
+    cantidad: int
+
+@app.get("/availability/", tags=["availability"])
+def get_availability(destination: Destino) -> List[Union[DisponibilidadTienda, DisponibilidadAlmacen]]:
+    conn = None
+    try:
+        if destination is Destino.TIENDA:
+            table = "DisponibilidadTienda"
+        elif destination is Destino.ALMACEN:
+            table = "DisponibilidadAlmacen"
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        statement = f"""
+        SELECT * FROM {table}
+        """
+        cur.execute(statement, [])
+        result = cur.fetchall()
+        cur.close()
+        # return a json with the result of the query
+        stock = [dict((cur.description[i][0], value)
+                         for i, value in enumerate(row)) for row in result]
+        return stock
+    except psycopg2.DatabaseError as e:
+        raise HTTPException(
+            status_code=500, detail=f"Error en lectura de stock: {e.pgerror}")
+    finally:
+        if conn is not None:
+            conn.close()
+
+@app.get("/availability/{id}/", tags=["availability"])
+def get_availability_by_id(destination: Destino, id: str) -> List[Union[DisponibilidadTienda, DisponibilidadAlmacen]]:
+    conn = None
+    try:
+        if destination is Destino.TIENDA:
+            table = "DisponibilidadTienda"
+            id_name = "ID_TIE"
+        elif destination is Destino.ALMACEN:
+            table = "DisponibilidadAlmacen"
+            id_name = "ID_ALM"
+        conn = get_db_connection()
+        cur = conn.cursor()
+        statement = f"""
+        SELECT * FROM {table}
+        WHERE {id_name} = %s;
+        """
+        cur.execute(statement, [id])
+        result = cur.fetchall()
+        cur.close()
+
+        # Check whether it is a valid warehouse id or not.
+        if not result:
+            raise ValueError()
+        stock = result
+
+        cur.close()
+        # return a json with the result of the query
+        stock = [dict((cur.description[i][0], value)
+                         for i, value in enumerate(row)) for row in result]
+        return stock
+    except ValueError:
+        raise HTTPException(
+            status_code=404, detail=f"{destination.value} {id} desconocido/a")
+    except psycopg2.DatabaseError as e:
+        raise HTTPException(
+            status_code=500, detail=f"Error en lectura de stock: {e.pgerror}")
+    finally:
+        if conn is not None:
+            conn.close()
+
+@app.post("/availability/", tags=["availability"])
+def create_availability(destination: Destino, availability: Union[DisponibilidadTienda, DisponibilidadAlmacen]):
+    conn = None
+    try:
+        if destination is Destino.TIENDA:
+            table = "DisponibilidadTienda"
+        elif destination is Destino.ALMACEN:
+            table = "DisponibilidadAlmacen"
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        statement = f"""
+        INSERT INTO {table}
+        VALUES (%s, %s, %s);
+        """
+        if isinstance(availability, DisponibilidadTienda):
+            id_value = availability.id_tie
+        else:
+            id_value = availability.id_alm
+        elements = [id_value, availability.id_prod, availability.cantidad]
+        cur.execute(statement, elements)
+        conn.commit()
+        cur.close()
+        return {"Creado correctamente": {"id": id_value}}
+    except psycopg2.DatabaseError as e:
+        raise HTTPException(
+            status_code=500, detail=f"Error en creación de stock: {e.pgerror}")
+    finally:
+        if conn is not None:
+            conn.close()
+
+@app.put("/availability/{id}/", tags=["availability"])
+def update_availability(destination: Destino, id: str, availability: DisponibilidadUpdate):
+    conn = None
+    try:
+        if destination is Destino.TIENDA:
+            table = "DisponibilidadTienda"
+            id_name = "ID_TIE"
+        elif destination is Destino.ALMACEN:
+            table = "DisponibilidadAlmacen"
+            id_name = "ID_ALM"
+        conn = get_db_connection()
+        cur = conn.cursor()
+        statement = f"""
+        UPDATE {table}
+        SET CANTIDAD = %s
+        WHERE {id_name} = %s AND ID_PROD = %s;
+        """
+        elements = [availability.cantidad, id, availability.id_prod]
+        cur.execute(statement, elements)
+        conn.commit()
+        cur.close()
+        return {"Actualizado correctamente": {"id": id}}
+    except ValueError:
+        raise HTTPException(
+            status_code=404, detail=f"{destination.value} {id} desconocido/a")
+    except psycopg2.DatabaseError as e:
+        raise HTTPException(
+            status_code=500, detail=f"Error en actualización de stock: {e.pgerror}")
+    finally:
+        if conn is not None:
+            conn.close()
+
+@app.delete("/availability/{id}/", tags=["availability"])
+def delete_availability(destination: Destino, id: str, id_prod: int):
+    conn = None
+    try:
+        if destination is Destino.TIENDA:
+            table = "DisponibilidadTienda"
+            id_name = "ID_TIE"
+        elif destination is Destino.ALMACEN:
+            table = "DisponibilidadAlmacen"
+            id_name = "ID_ALM"
+        conn = get_db_connection()
+        cur = conn.cursor()
+        statement = f"""
+        DELETE FROM {table}
+        WHERE {id_name} = %s AND ID_PROD = {id_prod};
+        """
+        cur.execute(statement, [id])
+        conn.commit()
+        cur.close()
+        return {"Eliminado correctamente": {"id": id}}
+    except ValueError:
+        raise HTTPException(
+            status_code=404, detail=f"{destination.value} {id} desconocido/a")
+    except psycopg2.DatabaseError as e:
+        raise HTTPException(
+            status_code=500, detail=f"Error en eliminación de stock: {e.pgerror}")
+    finally:
+        if conn is not None:
+            conn.close()
